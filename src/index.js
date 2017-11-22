5142873fa3ca6ed73e302c5149b82c1c4411163b
indianic <indianic@IND609>
2017-11-22 09:57:16 +0530

    merge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api'
-
+const requestUrl =
 const token = '457320898:AAF5Zv-Bw_rm2GHOdo2tyjcWv1etCU0NUTs';
 // Включить опрос сервера
 const bot = new TelegramBot(token, {polling: true});
@@ -7,7 +7,7 @@ const bot = new TelegramBot(token, {polling: true});
 // Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
 bot.onText(/\/getNew/, function (msg, match) {
     const fromId = msg.from.id;
-    const resp = ""
+    const resp = 
     bot.sendMessage(fromId, resp);
 });
 bot.onText(/\/start/, (msg) => {
